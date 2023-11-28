dad2725f20b257afe4acd9767c3142b1f1628a4b
Wenbo.Wu <3378814052@qq.com>
2023-11-28 10:45:12 +0800

    feat: ILendingPool

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,9 +1,31 @@
 import {getWeth} from "./getWeth";
+import {Signer} from "ethers";
+import {ethers, network} from "hardhat";
+import {NetWorkConfig} from "../config/network";
 
 async function main() {
+    const deployer = await ethers.provider.getSigner();
     await getWeth();
+    const lendingPool = await getLendingPool(deployer);
+    console.log("lendingPool",lendingPool);
 }
 
+async function getLendingPool(account: Signer) {
+    const chainId = network.config.chainId;
+    // 获取合约地址提供者
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        NetWorkConfig[chainId!].lendingPoolAddressesProvider,
+        account
+    );
+    // 获取 lendingPool 合约地址
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool();
+    return await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account
+    );
+}
 
 main()
     .then(() => process.exit(0))
